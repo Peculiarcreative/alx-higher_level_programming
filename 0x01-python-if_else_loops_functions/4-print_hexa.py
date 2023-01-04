14369e252cbf7cc01a631089a38756043f6bcf85
Peculiarcreative <peculiargrafiz30@gmail.com>
2023-01-04 12:47:23 -0800

    4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-number = 0
-while number < 99:
-print('{} = {}'.format(number, hex(number)))
-number = number + 1
+for number in range(0, 99):
+    print("{} = {}".format(number, hex(number)))
